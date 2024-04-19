6e2f4b4ac609bd8b7d55618e3ffd1bcc30301d6b
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-04-19 01:25:45 +0100

    added 2 and 3

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+This module contain an annotated function that returns
+the floor of a float
+'''
+
+
+def floor(n: float) -> int:
+    '''
+    takes in a float and returns the floot value of it
+    '''
+    return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+return the string representation of a string
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    Function takes argument 'n' and returns its string rep
+    '''
+
+    return str(n)
