52c3fa4d865d9bd070e2ac4dd2c9ae25746c7b8b
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-04-19 01:01:24 +0100

    initial commit

---
+++ 0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+This module contains a type annotated function that takes a float and
+returns their sum as a float
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    function add float 'a' and 'b' and returns float 'sum'
+    '''
+
+    return a + b
---
+++ 1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+This module contains a tyoe annotated function
+thats takes 2 strings and returns a concatenation of both
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Function retuns a concatenation 2 string argument
+    '''
+
+    return str1 + str2
