e6bae9e96d1d8eb0b104ed0d6b896c22c38c7a8d
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-04-24 00:27:29 +0100

    initial commit

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''Task 1's module.
+'''
+from typing import List
+from importlib import import_module as using
+
+
+async_generator = using('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    '''
+    Asynchronously generates and collects a list of
+    10 float numbers from an asynchronous generator.
+
+    Returns:
+        List[float]: A list containing 10 float numbers
+        generated asynchronously.
+
+    This function asynchronously iterates over the
+    'async_generator' from '0-async_generator' module
+    to yield 10 float numbers.
+    It then constructs and returns a list from these yielded numbers.
+    '''
+    return [num async for num in async_generator()]
