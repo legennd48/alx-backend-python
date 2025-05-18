47050d329e2b8f49036a4b07de99cf923665b873
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2025-05-18 01:32:42 +0100

    Update age filter in batch processing to include users over 25

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -28,7 +28,7 @@ def batch_processing(batch_size):
     """Process user data in batches."""
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
-            if user['age'] > 30:
+            if user['age'] > 25:
                 print(user) 
 
     
